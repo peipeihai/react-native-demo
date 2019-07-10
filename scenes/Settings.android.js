e720315f335bde17358c5d37404003fe61166001
barretyi <184515560@qq.com>
2019-07-10 23:41:54 +0800

    commit

--- scenes/Settings.android.js
+++ scenes/Settings.android.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableHighlight } from 'react-native';
 import { LoginButton } from '../components';
 
 export function Settings() {
+
+    const [counter, setCounter] = useState(0);
+
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>this is settings page for Android.</Text>
+            <Text style={styles.text}>
+                {counter}
+            </Text>
+
+            <TouchableHighlight  
+                style={styles.button} 
+                onPress={() => setCounter(counter + 1)}
+            >
+                <Text>increment</Text>
+            </TouchableHighlight>
+
+            <TouchableHighlight  
+                style={styles.button} 
+                onPress={() => setCounter(counter - 1)}
+            >
+                <Text>decrement</Text>
+            </TouchableHighlight>
+
+            <TouchableHighlight  
+                style={styles.button} 
+                onPress={() => setCounter(0)}
+            >
+                <Text>reset</Text>
+            </TouchableHighlight>
+
             <LoginButton />
         </View>
     );
@@ -20,8 +47,19 @@ const styles = StyleSheet.create({
     },
     text: {
         fontWeight: 'bold',
-        fontSize: 20,
+        fontSize: 30,
         color: '#f00',
         marginBottom: 30,
+    },
+    button: {
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: 'black',
+        paddingTop: 5,
+        paddingRight: 20,
+        paddingBottom: 5,
+        paddingLeft: 20,
+        borderRadius: 5,
+        marginBottom: 15,
     }
 })
